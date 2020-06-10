7c66298b5843370f8115c121548b19260e227c4b
Kevin Bradley <kevin.w.bradley@me.com>
2020-06-10 10:21:31 -0700

    initial commit

---
+++ System/Library/Frameworks/LocalAuthentication.framework/Support/DaemonUtils.framework/DaemonUtils.h
@@ -0,0 +1,7 @@
+#import <DaemonUtils/LANVRAM.h>
+#import <DaemonUtils/InstalledAppsCache.h>
+#import <DaemonUtils/Request.h>
+#import <DaemonUtils/EvaluationRequest.h>
+#import <DaemonUtils/CredentialRequest.h>
+#import <DaemonUtils/Caller.h>
+#import <DaemonUtils/DaemonUtils.h>
---
+++ System/Library/PrivateFrameworks/SafariFoundation.framework/SafariFoundation.h
@@ -0,0 +1,8 @@
+#import <SafariFoundation/SFSafariCredential.h>
+#import <SafariFoundation/SFDomainAssociationUtilities.h>
+#import <SafariFoundation/SFSafariCredentialStore.h>
+#import <SafariFoundation/SFSharedWebCredentialsDatabaseEntry.h>
+#import <SafariFoundation/SFSharablePasswordReceiver.h>
+#import <SafariFoundation/SFAutoFillFeatureManager.h>
+#import <SafariFoundation/_SFSharablePasswordEncryptionInformation.h>
+#import <SafariFoundation/SFSharablePassword.h>
